3f253ac3582402ad488ec051ae2b40d971a79237
Ashish Ranjan <148757537+AshShubh72@users.noreply.github.com>
2025-01-30 16:53:09 +0530

    Add files via upload

---
+++ Cipher_Qn2.java
@@ -0,0 +1,19 @@
+
+public class Cipher_Qn2 {
+    public static void main(String argus[]){
+        int a = 5;
+        double b = 1.26;
+        char c = 'a';
+        boolean d = true;
+
+        System.out.println("The int value " + a);
+        System.out.println("The char value " + c);
+        double newa = a;
+
+        int newchar = c;
+
+        System.out.println("The new after " + newa);
+        System.out.println("The new after " + newchar);
+
+    }
+}
